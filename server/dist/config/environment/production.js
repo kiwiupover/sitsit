648bbc3cc450c55aac1a2954a3ec3ab64dbdd4e1
Dave Laird <kiwiupover@gmail.com>
2016-03-20 15:30:17 -0700

    updated production port

--- server/dist/config/environment/production.js
+++ server/dist/config/environment/production.js
@@ -8,7 +8,7 @@ module.exports = {
           ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined,
 
           // Server port
-          port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
+          port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 80,
 
           // MongoDB connection options
           mongo: {
